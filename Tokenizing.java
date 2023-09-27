758e97e44d9e1b19a7afc87669f049848ce0b644
Rahul <1172002rahul@gmail.com>
2023-09-27 16:53:38 +0530

    Easy  Tokenizing problem Solved

---
+++ Tokenizing.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Tokenizing {
+    public static void main(String[] args) {
+       String input  = "Hello World";
+       int output = lengthOfLastWord(input);
+       System.out.println(output);
+    }
+
+    public static int lengthOfLastWord(String s) {
+        StringTokenizer z = new StringTokenizer(s, " ");
+        int count = z.countTokens();
+        String[] ans = new String[count];
+        int i = 0 ;
+          while(z.hasMoreTokens()){
+              ans[i] = z.nextToken();
+              i++;
+          }
+          String rw = ans[i-1];
+          return rw.length();
+        } 
+}
