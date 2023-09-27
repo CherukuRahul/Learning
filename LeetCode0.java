dca389d11a63efeb27434e6fdf9074a429fd8a05
Rahul <1172002rahul@gmail.com>
2023-09-27 15:44:49 +0530

    This is recursive approach limited for small imputs

---
+++ LeetCode0.java
@@ -0,0 +1,36 @@
+//https://leetcode.com/problems/decoded-string-at-index/?envType=daily-question&envId=2023-09-27
+public class LeetCode0 {
+    public static void main(String[] args) {
+        String s  = "a2345678999999999999999";
+        int k = 1;
+        String ans = "";
+        String anss = decodeAtIndex(s, k,ans);
+        System.out.println(anss);
+        String realans = "leetleetcodeleetleetcodeleetleetcode";
+        if(anss.equals(realans)){
+            System.out.println("reakl");
+        }
+    }
+
+    
+    public static  String decodeAtIndex(String s, int k,String ans) {
+    if(s.isEmpty()){
+       
+        return  Character.toString(ans.charAt(k-1));
+    }
+    char ch = s.charAt(0);
+    if(Character.isDigit(ch)){
+        String add = "";
+        for(int i = 1 ; i< Character.getNumericValue(ch); i++){
+            add += ans;
+        }
+        ans += add;
+        s = s.substring(1);
+    }else{
+        ans += ch;
+        s = s.substring(1);
+    }
+    return decodeAtIndex(s, k,ans);
+    }
+
+}
